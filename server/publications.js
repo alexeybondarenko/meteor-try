ad43c9fc36d6f1663f2fea37bc68d41e651970b5
alexeybondarenko <alexeybondarenko@me.com>
2015-09-03 20:14:35 +0300

    init

---
+++ server/publications.js
@@ -0,0 +1,8 @@
+
+// Meteor.publish('posts', function (author) {
+// 	return Posts.find({flagged: false, author: author});
+// });
+
+Meteor.publish('posts', function () {
+	return Posts.find({});
+})
